5bad974c646fefaad8d6955fe141cea582fbddcd
niC00L <nika.blanarova@gmail.com>
2015-08-19 21:20:46 +0200

    Added Create post, Fixed creating/editing posts

--- app/presenters/PostPresenter.php
+++ app/presenters/PostPresenter.php
@@ -3,114 +3,128 @@
 namespace App\Presenters;
 
 use Nette,
-	Nette\Application\UI\Form;
+    Nette\Application\UI\Form;
 
 class PostPresenter extends BasePresenter {
 
-	public function renderShow($address) {
-		$this->template->setFile(__DIR__ . '/templates/Post/showPost.latte');
+    public function renderShow($address) {
+        $this->template->setFile(__DIR__ . '/templates/Post/showPost.latte');
 
-		$post = $this->template->post = $this->database->table('page')->where('address', $address)->fetch();
-		$this['postForm']->setDefaults($post->toArray());
-	}
+        $post = $this->template->post = $this->database->table('page')->where('address', $address)->fetch();
+        $this['postForm']->setDefaults($post->toArray());
+    }
 
-	public function renderCategory($address) {
-		$this->template->setFile(__DIR__ . '/templates/Post/showCategory.latte');
+    public function renderCategory($address) {
+        $this->template->setFile(__DIR__ . '/templates/Post/showCategory.latte');
 
-		$category = $this->template->category = $this->database->table('page_ctg')->where('address', $address)->fetch();
-		$ctg_id = $this->database->table('page_ctg')->where('address', $address)->fetch()->id;
+        $category = $this->template->category = $this->database->table('page_ctg')->where('address', $address)->fetch();
+        $ctg_id = $this->database->table('page_ctg')->where('address', $address)->fetch()->id;
 
-		$page_id = $this->database->table('page_ctg_sort')->group('page_id')->having($ctg_id)->where('ctg_id', $ctg_id)->fetchAll();
-		$posts = $this->template->posts = $this->database->table('page')->where('id', $page_id)->fetchAll();
+        $page_id = $this->database->table('page_ctg_sort')->where('ctg_id', $ctg_id)->select('page_id');
+        $posts = $this->template->posts = $this->database->table('page')->where('id', $page_id)->fetchAll();
 
-		$this['postForm']->setDefaults($category->toArray());
-	}
+        $this['postForm']->setDefaults($category->toArray());
+    }
 
-	protected function createComponentPostForm() {
-		
-//		ak sa zobrazuje clanok vyberu sa data pre checkboxy s kategoriami
-		if ($this->getAction() == 'show') {
-			$address = $this->getParameter('address');
-			$id = $this->database->table('page')->where('address', $address)->fetch()->id;
+    protected function createComponentPostForm() {
 
-			$ctgs_in = $this->database->table('page_ctg_sort')->where('page_id', $id);
+//	ak sa zobrazuje clanok vyberie sa ktore checkboxy maju byt zaskrtnute
+        if ($this->getAction() == 'show') {
+            $address = $this->getParameter('address');
+            $id = $this->database->table('page')->where('address', $address)->fetch()->id;            
 
-			$ctgs = $this->database->table('page_ctg')->fetchAll();
-		}
-		
-		$form = new Form;
-		$form->addText('address', 'Adresa:')
-				->setRequired();
-		$form->addText('title', 'Titulok:')
-				->setRequired();
-		$form->addTextArea('description', 'Obsah:')
-				->setAttribute('class', 'materialize-textarea');
+            $ctgs_in = $this->database->table('page_ctg_sort')->where('page_id', $id);            
+        }
+        
+//      vyberu sa vsetky checkboxy
+        $ctgs = $this->database->table('page_ctg')->fetchAll();
 
-		if ($this->getAction() == 'show') {
-			$form->addTextArea('text', 'Obsah:')
-					->setAttribute('class', 'materialize-textarea');
+//        vytvori sa formular
+        $form = new Form;
+        $form->addText('address', 'Adresa:')
+                ->setRequired();
+        $form->addText('title', 'Titulok:')
+                ->setRequired();
+        $form->addTextArea('description', 'Popis:')
+                ->setAttribute('class', 'materialize-textarea');
 
-//			vykreslia sa checkboxy
-			foreach ($ctgs as $ctg) {
-				$form->addCheckbox('category_' . $ctg['id'], $ctg['title']);
-			}
-			
-//			pouzitym sa prida hodnota true
-			foreach ($ctgs_in as $ctg_in) {
-				$form->setValues(['category_' . $ctg_in->ctg_id => true]);
-			}
-		}
+        if ($this->getAction() == 'show' || $this->getAction() == 'createPost') {
+            $form->addTextArea('text', 'Obsah:')
+                    ->setAttribute('class', 'materialize-textarea');
 
-		$form->addSubmit('send', 'Uložit a publikovat')
-				->setAttribute('class', 'btn');
+//	vykreslia sa checkboxy
+            foreach ($ctgs as $ctg) {
+                $form->addCheckbox('category_' . $ctg['id'], $ctg['title']);
+            }
+        }
 
-		$form->onSuccess[] = array($this, 'postFormSucceeded');
-		return $form;
-	}
+//	pouzitym sa prida hodnota true
+        if ($this->getAction() == 'show') {
+            foreach ($ctgs_in as $ctg_in) {
+                $form->setValues(['category_' . $ctg_in->ctg_id => true]);
+            }
+        }
 
-	public function postFormSucceeded($form, $values) {
-		
-		if ($this->getAction() == 'show') {
-			$table = 'page';
-		} elseif ($this->getAction() == 'category') {
-			$table = 'page_ctg';
-		}
+        $form->addSubmit('send', 'Uložit a publikovat')
+                ->setAttribute('class', 'btn');
 
-		$address = $this->getParameter('address');
-		$id = $this->database->table($table)->where('address', $address)->fetch()->id;
+        $form->onSuccess[] = array($this, 'postFormSucceeded');
+        return $form;
+    }
 
-//		Vyradi checkboxy z values aby sa nezapisovali do tabulky pages
-		$ctg_sort = array();
-		foreach ($values as $key => $value) {
-			if (Nette\Utils\Strings::startsWith($key, 'category_')) {
+    public function postFormSucceeded($form, $values) {
+        $address = $this->getParameter('address');
+
+//	Vyradi checkboxy z values aby sa nezapisovali do tabulky pages
+        $ctg_sort = array();
+        foreach ($values as $key => $value) {
+            if (Nette\Utils\Strings::startsWith($key, 'category_')) {
 //				ak je checkbox zaskrtnuty vlozi jeho hodnotu do pola $ctg_sort
-				if ($values[$key] == true) {
+                if ($values[$key] == true) {
 //				do pola sa vlozia len cisla
-					array_push($ctg_sort, substr($key, -1));
-				}
-				unset($values[$key]);
-			}
-		}
+                    array_push($ctg_sort, substr($key, -1));
+                }
+                unset($values[$key]);
+            }
+        }
 
-//		zapisanie clankov a kategorii
-		$this->database->table('page_ctg_sort')->where('page_id', $id)->delete();
-		foreach ($ctg_sort as $ctg_id) {
-			$this->database->table('page_ctg_sort')->insert(array(
-				'page_id' => $id,
-				'ctg_id' => $ctg_id
-			));
-		}
-		
-//		zapisanie/pridanie obsahu stranky
-		if ($id) {
-			$post = $this->database->table($table)->get($id);
-			$post->update($values);
-		} else {
-			$post = $this->database->table($table)->insert($values);
-		}
 
-		$this->flashMessage('Uspesne publikovane.', 'success');
-		$this->redirect('this', ['address' => $values['address']]);
-	}
+        if ($address) {
+            if ($this->getAction() == 'show') {
+                $table = 'page';
+            } elseif ($this->getAction() == 'category') {
+                $table = 'page_ctg';
+            }
+
+            $id = $this->database->table($table)->where('address', $address)->fetch()->id;
+
+//	zapisanie/pridanie obsahu stranky
+            $post = $this->database->table($table)->get($id);
+            $post->update($values);
+        } else {
+            if ($this->getAction() == 'createPost') {
+                $table = 'page';
+            } elseif ($this->getAction() == 'createCategory') {
+                $table = 'page_ctg';
+            }
+            $post = $this->database->table($table)->insert($values);
+        }
+
+//	zapisanie clankov v kategoriach do spolocnej tabulky
+        if ($this->getAction() == 'show') {
+            $this->database->table('page_ctg_sort')->where('page_id', $id)->delete();
+        }                
+        
+        foreach ($ctg_sort as $ctg_id) {
+            $this->database->table('page_ctg_sort')->insert(array(
+                'page_id' => $post['id'],
+                'ctg_id' => $ctg_id
+            ));
+        }
+
+
+        $this->flashMessage('Uspesne publikovane.', 'success');
+        $this->redirect('this', ['address' => $values['address']]);
+    }
 
 }
