875adf810b987e6733176d93035751e53522f4df
niC00L <nika.blanarova@gmail.com>
2015-09-17 09:01:14 +0200

    Added global settings

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -3,39 +3,41 @@
 namespace App\Presenters;
 
 use Nette,
-	App\Model;
+    App\Model;
 
 /**
  * Base presenter for all application presenters.
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter {
 
-	/** @var Nette\Database\Context */
-	protected $database;
+    /** @var Nette\Database\Context */
+    protected $database;
 
-	public function __construct(\Nette\Database\Context $database) {
-		parent::__construct();
-		$this->database = $database;
-	}
-	
-	/** @var Nette\Http\SessionSection */
-	public $mySession;
-	
-	protected function startup() {
-		parent::startup();
-		// zaciatok session
-		$this->mySession = $this->session->getSection("mySession");
-	}
+    public function __construct(\Nette\Database\Context $database) {
+        parent::__construct();
+        $this->database = $database;
+    }
 
-	public function createComponentMenu() {
-		return new \App\Components\Menu\MenuControl($this->database);
-	}
+    /** @var Nette\Http\SessionSection */
+    public $mySession;
+    public $global;
+    
+    protected function startup() {
+        parent::startup();
+        // zaciatok session
+        $this->mySession = $this->session->getSection("mySession");
+        $this->template->global = $this->database->table('global_settings')->fetchPairs('setting_name', 'value');
+    }
 
-	public function beforeRender() {
-		parent::beforeRender();
+    public function createComponentMenu() {
+        return new \App\Components\Menu\MenuControl($this->database);
+    }
 
-		// ulozenie request
-		$this->mySession->backlink = $this->storeRequest();
-	}
+    public function beforeRender() {
+        parent::beforeRender();
+
+        // ulozenie request
+        $this->mySession->backlink = $this->storeRequest();
+    }
 
 }
