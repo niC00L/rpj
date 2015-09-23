1c17fd85953811c0f5e640e6baa38992cfabae78
niC00L <nika.blanarova@gmail.com>
2015-09-23 08:07:11 +0200

    Added global settings

--- app/AdminModule/presenters/AdminPresenter.php
+++ app/AdminModule/presenters/AdminPresenter.php
@@ -3,11 +3,39 @@
 namespace App\AdminModule\Presenters;
 
 use Nette,
-    App\Model;
+    App\Model,
+    Nette\Application\UI\Form,
+    Nette\Application\UI\Multiplier;
 
 class AdminPresenter extends \App\Presenters\BasePresenter {
 
     public function renderDefault() {
         
+    }
+
+    public function renderGlobal() {
+        $defaults = $this->template->globals = $this->database->table('global_settings')->where('editable', 1)->fetchAll();
+        foreach ($defaults as $def) {
+            $this['globalForm'][$def['id']]->setDefaults($def);
+        }
+    }
+
+    public function createComponentGlobalForm() {
+        return new Multiplier(function ($itemId) {
+            $form = new Form;
+            $form->addHidden('id');
+            $form->addText('value');
+            $form->addSubmit('delete', 'Upravit')
+                    ->setAttribute('class', 'btn');
+            $form->onSuccess[] = array($this, 'menuFormDeleteSucceeded');
+            return $form;
+        });
+    }
+    
+    public function menuFormDeleteSucceeded($form, $values) {
+        $this->database->table('global_settings')->where('id', $values['id'])->update($values);
+
+        $this->flashMessage('Polozka upravena.', 'success');
+        $this->redirect('Admin:global');
     }
 }
