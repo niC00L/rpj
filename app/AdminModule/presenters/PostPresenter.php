2e93b5bf5b5e1d8c29b6e4769b57536e9c7d0b90
niC00L <nika.blanarova@gmail.com>
2015-10-04 22:26:51 +0200

    Edited admin post presenter and template, now showing list of all categories and articles

--- app/AdminModule/presenters/PostPresenter.php
+++ app/AdminModule/presenters/PostPresenter.php
@@ -7,7 +7,32 @@ use Nette,
 
 class PostPresenter extends AdminPresenter {
 
-    protected function createComponentPostForm() {
+    public function renderDefault() {
+        $post_ctgs = $this->template->post_ctgs = $this->database->table('post_ctg')->where('status ? OR status ?', 1, 2)->fetchAll();
+        $posts = $this->template->posts = $this->database->table('post')->where('status ? OR status ?', 1, 2)->fetchAll();
+        $p_ids = $this->database->table('post_ctg_sort')->fetchAll();
+        $ids = array();
+        foreach ($p_ids as $p_id) {
+            $p_id->toArray();
+            $ids[$p_id['id']] = $p_id['post_id'];
+        }
+        $uncategorized = array();
+        foreach ($posts as $post_id) {
+            if (!in_array($post_id['id'], $ids)) {
+                $uncategorized[$post_id['id']] = $posts[$post_id['id']];
+            }
+        }
+        
+        $articles = array();
+        foreach ($post_ctgs as $ctgs) {
+            $articles[$ctgs['id']] = $this->database->table('post')->where('status ? OR status ?', 1, 2)->where(':post_ctg_sort.ctg_id', $ctgs['id'])->fetchAll();
+        }
+//        dump($articles); exit;
+        $this->template->posts = $articles;
+        $this->template->uncategorized = $uncategorized;
+    }
+
+    public function createComponentPostForm() {
 
 //      vyberu sa vsetky checkboxy
         $ctgs = $this->database->table('post_ctg')->where('status', 1)->fetchAll();
@@ -37,6 +62,12 @@ class PostPresenter extends AdminPresenter {
 
         $form->onSuccess[] = array($this, 'postFormSucceeded');
         return $form;
+    }
+
+    public function createComponentPostFormPublish() {
+        $form = new Form;
+        $form->addHidden('id');
+        $form->addSubmit('publish', 'Publikovat');
     }
 
     public function postFormSucceeded($form, $values) {
