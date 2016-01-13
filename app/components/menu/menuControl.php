e0f1a15e404b2a8452f96b7b1ce06ecb1cecd460
niC00L <nika.blanarova@gmail.com>
2016-01-13 09:15:32 +0100

    Edited menuControl

--- app/components/menu/menuControl.php
+++ app/components/menu/menuControl.php
@@ -4,21 +4,12 @@ namespace App\Components\Menu;
 
 use Nette\Application\UI\Control;
 
-class MenuControl extends Control {
+class MenuControl extends \App\AdminModule\Components\baseControl{
 
-	/** @var Nette\Database\Context */
-	private $database;
-	
-	public function __construct(\Nette\Database\Context $database) {
-		parent::__construct();
-		$this->database = $database;
-	} 
-	
 	public function render() {
 		$template = $this->template;
 		$template->setFile(__DIR__ . '/menuDefault.latte');
 		$this->template->links = $this->database->table('ctrl_menu')->order('order');
 		$template->render();
 	}
-
 }
