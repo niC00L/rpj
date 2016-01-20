a56c57174f585a1be6e314371b9d61227c2edba4
niC00L <nika.blanarova@gmail.com>
2016-01-20 08:55:34 +0100

    Added comment settings

--- app/AdminModule/presenters/AdminPresenter.php
+++ app/AdminModule/presenters/AdminPresenter.php
@@ -5,7 +5,8 @@ namespace App\AdminModule\Presenters;
 use Nette,
     App\Model,
     Nette\Application\UI\Form,
-    Nette\Application\UI\Multiplier;
+    Nette\Application\UI\Multiplier,
+    Nette\Utils\Strings;
 
 class AdminPresenter extends \App\Presenters\BasePresenter {
 
@@ -14,24 +15,39 @@ class AdminPresenter extends \App\Presenters\BasePresenter {
     }
 
     public function renderGlobal() {
-        $defaults = $this->template->globals = $this->database->table('global_settings')->where('editable', 1)->fetchAll();
+        $defaults = $this->database->table('global_settings')->where('editable', 1)->fetchAll();
+        $comments = array();
+        $other = array();
         foreach ($defaults as $def) {
-            $this['globalForm'][$def['id']]->setDefaults($def);
+            if (Strings::startsWith($def->setting_name, 'comment_')) {
+                $comments[$def->id] = $def;
+            } else {
+                $other[$def->id] = $def;                
+            }
         }
+        foreach($defaults as $d) {
+            $this['globalForm'][$d['id']]->setDefaults($d);
+        }
+        $this->template->globals = $other;
+        $this->template->comments = $comments;
     }
 
     public function createComponentGlobalForm() {
         return new Multiplier(function ($itemId) {
             $form = new Form;
             $form->addHidden('id');
-            $form->addText('value');
-            $form->addSubmit('delete', 'Upravit')
+            if ($itemId >= 8 && $itemId <= 10) {
+                $form->addCheckbox('value');
+            } else {
+                $form->addText('value');
+            }
+            $form->addSubmit('edit', 'Upravit')
                     ->setAttribute('class', 'btn');
             $form->onSuccess[] = array($this, 'menuFormDeleteSucceeded');
             return $form;
         });
     }
-    
+
     public function menuFormDeleteSucceeded($form, $values) {
         $this->database->table('global_settings')->where('id', $values['id'])->update($values);
 
