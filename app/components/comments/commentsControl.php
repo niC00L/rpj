9e708f459d279df7c6c0f4cdd02598659bc164d1
niC00L <nika.blanarova@gmail.com>
2016-04-04 09:50:33 +0200

    Edited comments

--- app/components/comments/commentsControl.php
+++ app/components/comments/commentsControl.php
@@ -9,16 +9,16 @@ class CommentsControl extends \App\AdminModule\Components\baseControl {
 
     private $id;
     private $table;
-    
+
     public function setForms($id, $table) {
-        $this->id = $id;        
+        $this->id = $id;
         $this->table = $table;
     }
-    
+
     public function renderShow($id, $table) {
         $template = $this->template;
         $template->setFile(__DIR__ . '/commentsDefault.latte');
-        $template->comments = $comments = $this->database->table('comments')->where($table.'_id', $id)->fetchAll();
+        $template->comments = $comments = $this->database->table('comments')->where($table . '_id', $id)->fetchAll();
         $template->render();
     }
 
@@ -33,16 +33,16 @@ class CommentsControl extends \App\AdminModule\Components\baseControl {
                 ->setAttribute('class', 'materialize-textarea');
         $form->addSubmit('submit', 'Odoslat')
                 ->setAttribute('class', 'btn');
-        
+
         $form->onSuccess[] = array($this, 'addCommentFormSuc');
         return $form;
     }
-    
+
     public function addCommentFormSuc($form, $values) {
 //        dump($this->id);exit;
-        $values[$this->table.'_id'] = $this->id;
+        $values[$this->table . '_id'] = $this->id;
         $this->database->table('comments')->insert($values);
-        
+
         $this->presenter->flashMessage('Komentar pridany.', 'success');
         $this->presenter->redirect('this');
     }
--- app/presenters/PostPresenter.php
+++ app/presenters/PostPresenter.php
@@ -10,26 +10,38 @@ class PostPresenter extends BasePresenter {
     public $table;
     public $defaults;
     public $id;
+    public $comments;
 
     public function setForms($id, $table, $defaults) {
+        
+    }
+
+    public function startup() {
+        parent::startup();
+        $this->comments = array(
+            'post' => $this->global['comment_post'],
+            'ctg' => $this->global['comment_ctg']
+        );
     }
     
-    public function actionShow($address) {                
-        $this->table = 'post';        
-        
+    public function actionShow($address) {
+        $this->table = 'post';
+
         $post = $this->template->post = $this->database->table('post')->where('address', $address)->fetch();
-        $images = $this->template->images = $this->database->table('imgs')->where(':img_sort.gallery_id', $post['gallery_id'])->fetchAll();
-        
+        $images = $this->template->images = $this->database->table('imgs')->where(':img_sort.gallery_id', $post['gallery_id'])->fetchAll();        
+
         $template = $this->database->table('site_templates')->where('site_templates.id', $post['template'])->fetch()->file_name;
         $this->setView($template);
-                
+        
+        $this->template->comments = $this->comments;
+
         $this->defaults = $post->toArray();
         $this->id = $post['id'];
-        
+
         if ($post->status != 1 && !$this->user->loggedIn) {
             $this->flashMessage('Prispevok bol odstraneny', 'danger');
             $this->redirect('Homepage:');
-        } else {            
+        } else {
             $this['editForm']->setForms($this->id, $this->table, $this->defaults);
             $this['deleteForm']->setForms($this->id, $this->table);
             $this['comments']->setForms($this->id, $this->table);
@@ -40,20 +52,23 @@ class PostPresenter extends BasePresenter {
         $this->table = 'post_ctg';
         $category = $this->template->category = $this->database->table('post_ctg')->where('address', $address)->fetch();
         $posts = $this->template->posts = $this->database->table('post')->where(':post_ctg_sort.ctg_id', $category['id'])->fetchAll();
-        
+
         $template = $this->database->table('site_templates')->where('site_templates.id', $category['template'])->fetch()->file_name;
         $this->setView($template);
         
+        $this->template->comments = $this->comments;
+
         $this->defaults = $category->toArray();
         $this->id = $category['id'];
-        
+
         if ($category->status != 1 && !$this->user->loggedIn) {
             $this->flashMessage('Kategoria bola odstranena', 'danger');
             $this->redirect('Homepage:');
-        } else {            
+        } else {
             $this['editForm']->setForms($this->id, $this->table, $this->defaults);
             $this['deleteForm']->setForms($this->id, $this->table);
             $this['comments']->setForms($this->id, $this->table);
         }
-    }    
+    }
+
 }
