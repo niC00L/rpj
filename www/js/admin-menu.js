cc7ba29f43fc907b8e3ac4333af39c3e622d4b8a
niC00L <nika.blanarova@gmail.com>
2016-04-09 22:41:22 +0200

    Edited menu

--- www/js/admin-menu.js
+++ www/js/admin-menu.js
@@ -1,5 +1,5 @@
 $(function () {
-    $(".button-collapse").sideNav({
+    $('.toggle-admin').sideNav({
         edge: 'right'
     });
 
--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,25 @@
 $(function () {
     $.nette.init();
-    $('.materialboxed').materialbox();    
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('select').material_select();
+
+    $('.toggle-menu').sideNav({
+        edge: $(this).data('side'), // Choose the horizontal origin
+    });
+
+    function readURL(input, output, bcg) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                if (bcg == 1) {
+                    $(output).css('background-image', 'url(' + e.target.result + ')');
+                } else {
+                    $(output).attr('src', e.target.result);
+                }
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
 });
