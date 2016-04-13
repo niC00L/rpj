c6a631b97f85169afaf29fbf8e77f4065fae929e
niC00L <nika.blanarova@gmail.com>
2016-04-13 23:53:17 +0200

    Minor edit profile

--- www/js/main.js
+++ www/js/main.js
@@ -9,17 +9,18 @@ $(function () {
         edge: $(this).data('side'), // Choose the horizontal origin
     });
 
-    function readURL(input, output, bcg) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                if (bcg == 1) {
-                    $(output).css('background-image', 'url(' + e.target.result + ')');
-                } else {
-                    $(output).attr('src', e.target.result);
-                }
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
 });
+
+function readURL(input, output, bcg) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            if (bcg == 1) {
+                $(output).css('background-image', 'url(' + e.target.result + ')');
+            } else {
+                $(output).attr('src', e.target.result);
+            }
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
