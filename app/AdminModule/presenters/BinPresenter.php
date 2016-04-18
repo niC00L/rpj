77055e3e47ba89f271d02e5f4f4ee41e7e1827cc
niC00L <nika.blanarova@gmail.com>
2016-04-18 18:47:44 +0200

    Edited Bin

--- app/AdminModule/presenters/BinPresenter.php
+++ app/AdminModule/presenters/BinPresenter.php
@@ -19,9 +19,12 @@ class BinPresenter extends AdminPresenter {
         }
     }
 
-    public function renderDefault() {
+    public function actionDefault() {
         $posts = $this->template->posts = $this->database->table('post')->where('status', 0)->fetchAll();
         $post_ctgs = $this->template->post_ctgs = $this->database->table('post_ctg')->where('status', 0)->fetchAll();
+        $comments = $this->template->comments = $this->database->table('comments')->where('status', 0)->fetchAll();
+        $this->template->author = $author = $this->database->table('users')->fetchAll();
+        $controls = $this->template->controls = \App\AdminModule\Presenters\ControlsPresenter::getControls($this->database, 0)[0];
     }
 
     public function createComponentBinRenew() {
