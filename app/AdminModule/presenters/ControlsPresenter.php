f96c4e62eba9c3a3b19850257403b4bf9ceda6ab
niC00L <nika.blanarova@gmail.com>
2016-04-18 17:54:27 +0200

    Added components presenter

---
+++ app/AdminModule/presenters/ControlsPresenter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use Nette,
+    App\Model;
+
+class ControlsPresenter extends AdminPresenter {
+
+    public function startup() {
+        parent::startup();
+        if ($this->getUser()->getRoles()[0] == 'banned') {
+            $this->flashMessage('Máte ban');
+            $this->redirect('Admin:default');
+        } elseif (!$this->getUser()->isAllowed('post', 'edit')) {
+            $this->flashMessage('Nemáte prístup k tejto stránke');
+            $this->redirect('Admin:default');
+        }
+    }
+    
+    static function getControls($db, $status) {
+        $controls = $db->table('controls')->where('status', $status)->where('editable', 1)->fetchAll();
+        $links = array();
+        foreach ($controls as $item) {
+            if (!in_array($item['position'], $links)) {
+                $link = explode('-', $item['position']);
+                if ($link[0] == 'layout') {
+                    $links[$item['position']] = '#';
+                } elseif ($link[0] == 'post') {
+                    if($link[1] == 'show') {
+                        $link[2] = $this->database->table('post')->where('status', 1)->fetch()->address;
+                    }
+                    else {
+                        $link[2] = $this->database->table('post_ctg')->where('status', 1)->fetch()->address;
+                    }
+                    $links[$item['position']] = array( \Nette\Utils\Strings::capitalize($link[0]), \Nette\Utils\Strings::capitalize($link[1]), $link[2]);
+                } else {
+                    $links[$item['position']] = array( \Nette\Utils\Strings::capitalize($link[0]), \Nette\Utils\Strings::capitalize($link[1]));
+                }
+            }
+        }
+        return array($controls, $links);
+    }
+
+    public function actionDefault() {        
+        $controls = $this::getControls($this->database, 2)[0];
+        $links = $this::getControls($this->database, 2)[1];
+        $this->template->controls = $controls;
+        $this->template->links = $links;
+    }
+
+}
