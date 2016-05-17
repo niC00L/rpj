3638586d98828e76432adf86f5d8b248fc1a830a
niC00L <nika.blanarova@gmail.com>
2016-05-17 09:32:48 +0200

    Added console hack for changimg menu

--- www/js/admin-menu.js
+++ www/js/admin-menu.js
@@ -1,25 +1,42 @@
 $(function () {
-    $('.toggle-admin').sideNav({
-        edge: 'right'
-    });
+	$('.toggle-admin').sideNav({
+		edge: 'right'
+	});
 
-    var x = $.cookie('menu');
+	var x = $.cookie('menu');
 
-    $("header").addClass(x);
-    $("main").addClass(x);
-    $("#admin-nav").addClass(x);
+	$("header").addClass(x);
+	$("main").addClass(x);
+	$("#admin-nav").addClass(x);
 
-    $("#admin-toggle").click(function () {   
-        $("header, main, #admin-nav").addClass('w-trans');        
-        $("header").toggleClass('admin-open');
-        $("main").toggleClass('admin-open');
-        $("#admin-nav").toggleClass('admin-open');
-        
-        if ($("#admin-nav").hasClass('admin-open')) {
-            $.cookie('menu', 'admin-open', { path: '/' });
-        }
-        else {
-            $.cookie('menu', 'admin-closed', { path: '/' });
-        }                        
-    });    
+	$("#admin-toggle").click(function () {
+		$("header, main, #admin-nav").addClass('w-trans');
+		$("header").toggleClass('admin-open');
+		$("main").toggleClass('admin-open');
+		$("#admin-nav").toggleClass('admin-open');
+
+		if ($("#admin-nav").hasClass('admin-open')) {
+			$.cookie('menu', 'admin-open', {path: '/'});
+		}
+		else {
+			$.cookie('menu', 'admin-closed', {path: '/'});
+		}
+	});
+
+	var change = $.cookie('changeMenu');
+	changeMenu(change);
 });
+
+function changeMenu(rights) {
+	if(!rights) {
+		rights = '';
+	}
+	$.cookie('changeMenu', rights, {path: '/'});
+	$('#admin-nav li').each(function (li) {
+		$(this).show();
+		if (rights && !$(this).hasClass(rights)) {
+			$(this).hide();
+		}
+	});
+}
+;
